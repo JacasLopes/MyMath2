dfee29335048f4f0776ed3398d4815ff99bf91fb
JacasLopes <joaquim.lopes.duarte@gmail.com>
2021-06-09 14:52:12 +0100

    First Commit

---
+++ MyMath2/__init__.py
@@ -0,0 +1,3 @@
+from MyMath2.arithmetic import *
+from MyMath2.advanced.fib import *
+from MyMath2.advanced.sqrt import *
---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='MyMath2',
+      version='1.0',
+      description='MyMath Distribution Package',
+      author='Your Name',
+      author_email='Your Email',
+      url='The URL project - github for example',
+      packages=['MyMath2'],
+     )
